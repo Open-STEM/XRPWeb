b10c1c53a2cfc46d35761d9bcd9d4456f1ccdee7
Kevin Quan <kcq@yahoo.com>
2024-12-18 16:57:21 -0500

    Added block to python test for now

--- src/app/components/blockly.tsx
+++ src/app/components/blockly.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import * as Blockly from 'blockly/core';
+import {pythonGenerator} from 'blockly/python';
 import { BlocklyWorkspace } from 'react-blockly'
 import BlocklyConfigs from './blockly/xrp_blockly_configs';
 
 type Props = {}
 
 function BlocklyEditor({}: Props) {
+  function onWorkspaceDidChange(ws) {
+    const code = pythonGenerator.workspaceToCode(ws);
+    console.log(code);
+  };
+
   return (
     <BlocklyWorkspace
         className="h-full" // you can use whatever classes are appropriate for your app's CSS
@@ -18,6 +25,7 @@ function BlocklyEditor({}: Props) {
             },
         }}
         initialJson={BlocklyConfigs.InitialJson}
+        onWorkspaceChange={onWorkspaceDidChange}
     />
   )
 }
