e612d4ed5a54559799afd49024ba5a65117d4e5b
fgrossman <fgrossman@gmail.com>
2025-01-30 17:40:11 -0500

    New managers

---
+++ src/managers/commandstoxrpmgr.ts
@@ -0,0 +1,33 @@
+/**
+ * CommandsToXRPMgr - manages routines that send commands to the XRP REPL
+ **/ 
+export class CommandToXRPMgr {
+    public CommandsToXRPMgr() {
+        //constructor
+        
+        //resetTerminal
+        //clearIsRunning
+        //batteryVoltage
+        //getVersionInfo
+
+        //getOnBoardFSTree
+        //deleteFileOrDir
+        //renameFile
+        //downloadFile
+        //buildPath
+        //uploadFile
+             //uploadFiles
+        //getFileContents
+        //checkFileExists (do we need this?)
+
+        //updateMainFile
+        //executeLines
+        //resetBot
+
+        
+        
+
+    
+
+    }
+}
---
+++ src/managers/xrpdatamgr.ts
@@ -0,0 +1,14 @@
+/**
+ * XRPDataMgr - Manages data being sent or received from the XRP
+ */
+export class XRPDataMgr {
+    public XRPDataMgr() {
+        //constructor
+        
+        //readloop - should be a worker
+        //WriteToDevice
+
+        //SendTerminalKbd
+
+    }
+}
