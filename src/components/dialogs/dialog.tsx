328189faafbdf028f7bd945eaa02d73ee5a92efc
Kevin Quan <kcq@yahoo.com>
2025-11-08 16:52:11 -0500

    Addressed issue #120

--- src/components/dialogs/dialog.tsx
+++ src/components/dialogs/dialog.tsx
@@ -11,7 +11,7 @@ const Dialog = forwardRef<HTMLDialogElement, DialogProps>((dlgProps, ref) => {
         <dialog
             style={{zIndex: 1000, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0}}
             open={dlgProps.isOpen}
-            className="rounded-md shadow-md border-shark-400 backdrop:bg-mountain-mist-500 backdrop:opacity-40 dark:border dark:border-shark-600 dark:bg-shark-950 transition-all"
+            className="rounded-md shadow-md border-shark-400 backdrop:bg-mountain-mist-500 backdrop:opacity-40 dark:border dark:border-shark-600 dark:bg-shark-950 transition-all overflow-hidden"
             ref={ref}
             onClick={(e) => {
                 if (e.currentTarget === e.target) {
