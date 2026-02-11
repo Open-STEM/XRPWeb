f86f65fe25bf272ba6c96353d61ff9b93710f193
Kevin Quan <kcq@yahoo.com>
2026-02-11 15:14:17 -0500

    Addressed issue 223

--- src/managers/editormgr.ts
+++ src/managers/editormgr.ts
@@ -492,6 +492,8 @@ export default class EditorMgr {
             return; // Can't save if not connected
         }
 
+        const isLogin = AppMgr.getInstance().authService.isLogin;
+
         // Get all unsaved editor sessions
         const unsavedSessions: Array<{ id: string; session: EditorSession }> = [];
         this.editorSessions.forEach((session, id) => {
@@ -523,6 +525,13 @@ export default class EditorMgr {
                     const content = session.content
                     
                     if (content) {
+                        if (isLogin && session.gpath) {
+                            // If Google user login, save to Google drive
+                            const mineType = session.type === EditorType.PYTHON ? 'text/x-python' : 'application/json';
+                            const blob = new Blob([content], { type: mineType});
+                            await AppMgr.getInstance().driveService.upsertFileToGoogleDrive(blob, fileName, mineType, session.gpath);
+                        }
+
                         // Save the editor
                         await CommandToXRPMgr.getInstance().uploadFile(session.path, content, true);
                         
