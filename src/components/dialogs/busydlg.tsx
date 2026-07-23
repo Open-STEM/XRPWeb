d610136e6e192317a7fc061b2bae6c12caa3dcb5
Kevin Quan <kcq@yahoo.com>
2026-07-23 11:20:54 -0400

    Fixed issue 272

--- src/components/dialogs/busydlg.tsx
+++ src/components/dialogs/busydlg.tsx
@@ -1,3 +1,4 @@
+import useResizeObserver from 'use-resize-observer';
 import XRPBusyImage from '@assets/images/logo_x_rotate_in_place.gif';
 
 type BusyDlgProps = {
@@ -10,11 +11,16 @@ type BusyDlgProps = {
  * @returns 
  */
 function BusyDialog({title}: BusyDlgProps) {
+  const { ref, width = 0 } = useResizeObserver<HTMLDivElement>();
+
+  // Scale image based on container width
+  const imageSizeClass = width >= 500 ? 'h-32 w-auto' : 'h-24 w-auto';
+
   return (
     <div className="flex flex-col gap-4 items-center p-4">
         <h2 className='text-lg font-bold text-mountain-mist-700 dark:text-mountain-mist-300'>{title}</h2>
-        <div className="flex flex-col items-center border-shark-800 dark:border-shark-500 dark:bg-shark-950 h-48 rounded-md border p-8 md:w-[500px] xl:w-[600px]">
-            <img src={XRPBusyImage} alt="busy" className="self-center h-32 w-auto" />
+        <div ref={ref} className="flex flex-col items-center border-shark-800 dark:border-shark-500 dark:bg-shark-950 h-48 rounded-md border p-8 md:w-[500px] xl:w-[600px]">
+            <img src={XRPBusyImage} alt="busy" className={`self-center ${imageSizeClass}`} />
         </div>
     </div>
   )
